27b922dd8115284151f24c70793bef1fb8965b8e
MACAO979 <marthacastelen@gmail.com>
2026-05-04 15:50:58 +0200

    Update map coordinates and marker icon

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,36 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-var map = L.map('map').setView([51.317, 4.933], 10);
+var map = L.map('map').setView([ 51.323278, 4.938841], 10);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { 
                                 maxZoom: 19,
                                 attribution: '&copy; <a href="contact.html">MACServices Address Map</a>'
                         }).addTo(map);
 
-var marker = L.marker([51.317, 4.933]).addTo(map);
+var logoIcon = L.icon({
+    iconUrl: 'assets/logo marker.png',
+    shadowUrl: 'assets/logo marker shadow.png',
 
-var circle = L.circle([51.317, 4.940], {
-    color: 'red',
-    fillColor: 'rgb(201, 31, 31)',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
+    iconSize:     [55, 35], // size of the icon
+    shadowSize:   [7, 4], // size of the shadow
+    iconAnchor:   [45, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});                        
+var marker = L.marker([51.323278, 4.938841], {icon: logoIcon}).addTo(map);
+
+var bounds = [[51.3095339, 4.9211096], [51.32471998427867, 4.9691895838]];
+L.rectangle(bounds, {color: "#34594f", weight: 1}).addTo(map);
+
+
 
 var polygon = L.polygon([
-    [51.317, 4.933],
-    [51.315, 4.931],
-    [51.31, 4.93],
+   [51.323, 4.938],
+    [51.323, 4.937],
+    [51.32, 4.93],
 ]).addTo(map);
 marker.bindPopup("<b>Here is were</b><br> You can find us!").openPopup();
-circle.bindPopup("This is the area around us.");
+rectangle.bindPopup("This is the area around us.");
 polygon.bindPopup(".");
 
 var dt = new Date();
